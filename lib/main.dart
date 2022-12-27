5a1ed1c8334b4995de14c125ba888f2a3ae32ee2
weriks <ti15@viacometa.com.br>
2022-12-26 20:52:53 -0400

    refactor: importa rotas auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
+import 'package:vakinha_burger_mobile/app/routes/auth_routers.dart';
 import 'package:vakinha_burger_mobile/app/routes/splash_routers.dart';
 
 void main() {
@@ -15,7 +16,10 @@ class VakinhaBurguerMainApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Vakinha Burguer',
       theme: VakinhaUI.theme,
-      getPages: [...SplashRouters.routers],
+      getPages: [
+        ...SplashRouters.routers,
+        ...AuthRouters.routers,
+      ],
     );
   }
 }
