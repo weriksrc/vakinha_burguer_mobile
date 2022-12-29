ac0264958f76910ac96b464d7e09bb45ef3007aa
weriks <ti15@viacometa.com.br>
2022-12-29 07:39:07 -0400

    refactor: veficica se usuario esta logado

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
+import 'package:vakinha_burger_mobile/app/modules/splash/splash_controller.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends GetView<SplashController> {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
@@ -37,10 +38,8 @@ class SplashPage extends StatelessWidget {
                       label: 'Acessar',
                       width: context.widthTransformer(reducedBy: 40),
                       height: 35,
-                      onPressed: () {
-                        Get.toNamed('/auth/login');
-                      },
-                    )
+                      onPressed: () => controller.checkLogged(),
+                    ),
                   ],
                 ),
               )
