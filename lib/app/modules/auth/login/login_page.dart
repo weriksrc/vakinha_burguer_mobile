5df056e11e88e7c5e49330d011366bdca64fd610
weriks <ti15@viacometa.com.br>
2022-12-29 07:40:34 -0400

    refactor: adiciona controller e validator nos campo do formulario

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/vakinha_state.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
+import 'package:vakinha_burger_mobile/app/modules/auth/login/login_controller.dart';
+import 'package:validatorless/validatorless.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends VakinhaState<LoginPage, LoginController> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailEC.dispose();
+    _passwordEC.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,61 +35,86 @@ class LoginPage extends StatelessWidget {
       ),
       backgroundColor: Colors.white,
       body: LayoutBuilder(builder: (_, constraints) {
-        return ConstrainedBox(
-          constraints: BoxConstraints(
-            minHeight: constraints.maxHeight,
-          ),
-          child: IntrinsicHeight(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Form(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Login',
-                      style: context.textTheme.headline6?.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: context.theme.primaryColorDark),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const VakinhaTextformfield(label: 'E-mail'),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    const VakinhaTextformfield(
-                      label: 'Senha',
-                      obscuteText: true,
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    Center(
-                      child: VakinhaButton(
-                        width: context.width,
-                        label: 'Entrar',
-                        onPressed: () {},
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+            ),
+            child: IntrinsicHeight(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
+                        style: context.textTheme.headline6?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: context.theme.primaryColorDark),
                       ),
-                    ),
-                    const Spacer(),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Não possui uma conta?'),
-                        TextButton(
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      VakinhaTextformfield(
+                        label: 'E-mail',
+                        controller: _emailEC,
+                        validator: Validatorless.multiple([
+                          Validatorless.required('E-mail obrigatório'),
+                          Validatorless.email('E-mail inválido')
+                        ]),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      VakinhaTextformfield(
+                        label: 'Senha',
+                        controller: _passwordEC,
+                        validator: Validatorless.multiple([
+                          Validatorless.required('Senha obrigatória'),
+                          Validatorless.min(
+                              6, 'Senha deve conter pelo menos 6 caracteres')
+                        ]),
+                        obscuteText: true,
+                      ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Center(
+                        child: VakinhaButton(
+                          width: context.width,
+                          label: 'Entrar',
                           onPressed: () {
-                            Get.toNamed('/auth/register');
+                            final formValid =
+                                _formKey.currentState?.validate() ?? false;
+                            if (formValid) {
+                              controller.login(
+                                email: _emailEC.text,
+                                password: _passwordEC.text,
+                              );
+                            }
                           },
-                          child: const Text(
-                            'Cadastre-se',
-                            style: VakinhaUI.textBold,
-                          ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Não possui uma conta?'),
+                          TextButton(
+                            onPressed: () {
+                              Get.toNamed('/auth/register');
+                            },
+                            child: const Text(
+                              'Cadastre-se',
+                              style: VakinhaUI.textBold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
