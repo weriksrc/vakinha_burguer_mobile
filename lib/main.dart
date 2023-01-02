66be064bf1155022167ce45045aec51d765ca57a
weriks <ti15@viacometa.com.br>
2023-01-02 13:54:32 -0400

    refactor: importa routers

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:vakinha_burger_mobile/app/core/bindings/application_binding.dart
 import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
 import 'package:vakinha_burger_mobile/app/routes/auth_routers.dart';
 import 'package:vakinha_burger_mobile/app/routes/home_routers.dart';
+import 'package:vakinha_burger_mobile/app/routes/product_routers.dart';
 import 'package:vakinha_burger_mobile/app/routes/splash_routers.dart';
 
 Future<void> main() async {
@@ -25,6 +26,7 @@ class VakinhaBurguerMainApp extends StatelessWidget {
         ...SplashRouters.routers,
         ...AuthRouters.routers,
         ...HomeRouters.routers,
+        ...ProductRouters.routers,
       ],
     );
   }
