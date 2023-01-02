a4019770a252447fbfe9115cb618c2098a98bd92
weriks <ti15@viacometa.com.br>
2023-01-02 13:51:01 -0400

    feat: cria product detail controller

---
+++ lib/app/modules/product_detail/product_detail_controller.dart
@@ -0,0 +1,32 @@
+import 'package:get/get.dart';
+import 'package:vakinha_burger_mobile/app/models/product_model.dart';
+
+class ProductDetailController extends GetxController {
+  final _product = Rx<ProductModel>(Get.arguments);
+  final _totalPrice = 0.0.obs;
+  final _quantity = 1.obs;
+
+  ProductModel get product => _product.value;
+  double get totalPrice => _totalPrice.value;
+  int get quantity => _quantity.value;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _totalPrice(product.price);
+
+    ever<int>(_quantity, (quantity) {
+      _totalPrice(product.price * quantity);
+    });
+  }
+
+  void addProduct() {
+    _quantity.value += 1;
+  }
+
+  void removeProduct() {
+    if (_quantity > 1) {
+      _quantity.value -= 1;
+    }
+  }
+}
